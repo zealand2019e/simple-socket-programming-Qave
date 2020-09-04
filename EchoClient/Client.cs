bc6ed690d74c3770ce3e70261dd9d7b61d4dfb66
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2020-09-04 12:05:25 +0200

    EchoClient

---
+++ EchoClient/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+
+            using (TcpClient socket = new TcpClient("localhost", 7))
+            {
+                NetworkStream ns = socket.GetStream();
+
+                StreamReader streamReader = new StreamReader(ns);
+                StreamWriter streamWriter = new StreamWriter(ns);
+
+                streamWriter.WriteLine("Lol");
+                streamWriter.Flush();
+
+
+                string line = streamReader.ReadLine();
+                Console.WriteLine(line);
+
+
+            }      
+            
+
+        }
+    }
+}
---
+++ EchoClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+            Console.ReadKey();
+        }
+    }
+}
